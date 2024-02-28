d33745e321a2eff15723316a1ad08f37329f3d8e
manu989898 <apkmanu1@gmail.com>
2024-02-28 18:48:00 +0100

    cositas nuevas.

---
+++ EjemplosCtrolVersionesGit/src/com/apruebame/pablo/PabloApruebameLoNecvesito.java
@@ -0,0 +1,11 @@
+package com.apruebame.pablo;
+
+public class PabloApruebameLoNecvesito {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Las 11 razones por las que Pablo es el mejor profesor del planeta:");
+		
+	}
+
+}
